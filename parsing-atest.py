b863eb7fbbb926a4ea619dcd2c37d3f944a7a6cf
guillaumerosin <guillaumerosin@users.noreply.github.com>
2026-03-03 10:34:35 +0100

    up

--- parsing-atest.py
+++ parsing-atest.py
@@ -144,6 +144,11 @@ def read_kafka_for_scylla():
         # value_deserializer=lambda x: json.loads(x.decode("utf-8")),  # c'est pas du json debilous
         consumer_timeout_ms=10000, 
     )
+    for msg in consumer:
+        raw = raw = msg.value.decode("utf-8", errors="replace")
+        log.info("Message brut: %s", raw)
+        parts = raw.split("\t")
+        
     messages = [msg.value for msg in consumer]
     consumer.close()
 
